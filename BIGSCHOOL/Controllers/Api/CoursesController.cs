715194ab2618fffdcbb591b2a3663052e107e54f
Tee Dijnn <dinhhuynhtue@gmail.com>
2019-04-10 11:46:10 +0700

    done! Cancel chưa test !!!

---
+++ BIGSCHOOL/Controllers/Api/CoursesController.cs
@@ -0,0 +1,36 @@
+﻿using BIGSCHOOL.Models;
+using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace BIGSCHOOL.Controllers.Api
+{
+    public class CoursesController : ApiController
+    {
+        public ApplicationDbContext dbContext;
+
+        public CoursesController()
+        {
+            dbContext = new ApplicationDbContext();
+        }
+
+        [HttpDelete]
+        public IHttpActionResult Cancel(int id)
+        {
+            var userId = User.Identity.GetUserId();
+            var course = dbContext.Courses.Single(c => c.Id == id && c.LecturerId == userId);
+
+            if (course.IsCancled)
+                return NotFound();
+            course.IsCancled = true;
+            dbContext.SaveChanges();
+
+            return Ok();
+       
+        }
+    }
+}
